40998cbff6ba39c18e3f1adf00097ae3ddb5c506
DjentleVibe <116117854+DjentleViBe@users.noreply.github.com>
2025-06-25 00:55:37 +0200

    config large

--- config.py
+++ config.py
@@ -5,10 +5,10 @@
 # 1 : eval
 # 2 : both 
 # 3 : load from file
-MODE                =   1
-BACKUP              =   "dec_only_notes_CB_Small"
-SAVE                =   "dec_only_notes_CB_Small_A"
-START_ID            =   9437
+MODE                =   0
+BACKUP              =   "dec_only_notes_CB_Large_2"
+SAVE                =   "dec_only_notes_CB_Large_2A"
+START_ID            =   8125
 BOS_TRUE            =   0
 EOS_TRUE            =   1
 # 0 - Vanilla
@@ -17,22 +17,22 @@ EOS_TRUE            =   1
 # 3 - Same as above + last note of every bar is fed to firt note of next bar
 # 4 - Same as above + loop until no repeatitive notes > BOS 
 TEST_CRITERIA       =   3
-TEST_TRIES          =   5
+TEST_TRIES          =   1
 # 1 - Greedy Search
 # 2 - Beam Search
 # 3 - Multinomial Sampling
 # 4 - Multinomial Sampling - manual
 PREDICTION_CRITERIA =   3
 ########## Params ##############
-EPOCHS          =   2
-SAVE_EVERY      =   100
+EPOCHS          =   100
+SAVE_EVERY      =   20
 VOCAB_SIZE      =   23210
-FFN_HIDDEN      =   512
+FFN_HIDDEN      =   1024
 MAX_SEQ_LENGTH  =   32
-NUM_HEADS       =   4
+NUM_HEADS       =   8
 DROP_PROB       =   0.2
 NUM_LAYERS      =   6
-D_MODEL         =   128
+D_MODEL         =   256
 SCHEDULER       =   1
 SCHEDULER_SIZE  =   30
 LEARNING_RATE   =   0.00005
@@ -40,10 +40,10 @@ SMOOTHING       =   0.1
 PATCH           =   1
 STRIDE          =   1
 TRAINING        =   ["CB"]
-NUM_SEQUENCE    =   3104
+NUM_SEQUENCE    =   6208
 CONVERGENCE     =   0.0005
-TEMPERATURE     =   0.1
-BATCH           =   16
+TEMPERATURE     =   1.0
+BATCH           =   32
 
 EOS             =   23185
 BOS             =   23186
