9e4e8e0789407f5ecda1644b6dde335794af8855
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2026-01-02 14:08:37 +0100

    validation

--- validation.py
+++ validation.py
@@ -1,12 +1,14 @@
 import torch
 import config as cfg
 from masking import create_combined_mask
+from _loss.customloss import ValidationLoss
 
 def validation(loader, device, optimizer, embedding_layer, decoder, criterion, pos_enc):
     decoder.eval()
     epoch_loss = 0.0
     batch_count = 0
     prev_sequence_embedding = None
+    validation_criterion = ValidationLoss()
     with torch.no_grad():
         for batch_token_ids, batch_target in loader:
             #if len(batch_token_ids) != cfg.BATCH:
@@ -21,14 +23,13 @@ def validation(loader, device, optimizer, embedding_layer, decoder, criterion, p
             input_embeddings = embeddings + pos_enc[:batch_token_ids.size(1)]
 
             # Forward
-            logits = decoder(input_embeddings, mask)
-
+            logits, *_ = decoder(input_embeddings, mask)
+            logits = logits.view(-1, logits.size(-1))
             # Flatten target
             batch_target = batch_target.view(-1)
 
             # Compute loss
-            loss, ce_loss, rep_loss, kl_loss, prev_sequence_embedding = criterion(
-                logits, batch_target, prev_sequence_embedding, cfg.EPOCHS)
+            loss = validation_criterion(logits, batch_target)
             epoch_loss += loss.item()
             batch_count += 1
         avg_loss = epoch_loss / batch_count
